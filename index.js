9925ba97c51706b2dbef422861f3e5eccbe6675d
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-07-11 18:13:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string = string.toLowerCase) {
-    return "I can't hear you!"
-  } else if (string = string.toUpperCase && "I LOVE YOU, GRANDMA") {
+  if (string = string.toLowerCase && "I love you, Grandma.") {
     return "I love you, too."
+  } else if (string = string.toLowerCase) {
+    return "I can't hear you!"
   } else if (string = string.toUpperCase) {
     return "YES INDEED!"
   }
